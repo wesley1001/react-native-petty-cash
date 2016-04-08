2f755a1029a65c9f623bfcbf36feb9a63847e40f
Neung <neungszad@gmail.com>
2016-04-08 11:19:27 +0700

    Add category payment picker

--- app/components/ExpenseAdd.js
+++ app/components/ExpenseAdd.js
@@ -1,4 +1,4 @@
-import React, {Component, StyleSheet, Text, View, TextInput} from 'react-native';
+import React, {Component, StyleSheet, Text, View, TextInput, Picker} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import Navbar from './widgets/Navbar';
 var Icon = require('react-native-vector-icons/FontAwesome');
@@ -6,27 +6,40 @@ var Icon = require('react-native-vector-icons/FontAwesome');
 class ExpenseAdd extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        language: ''
+      }
     }
 
     render() {
+      const CATEGORIES = [
+        {label: "ค่าเดินทาง", value: "transport"},
+        {label: "สวัสดิการ (ทำฟัน)", value: "welfare_tooth"},
+        {label: "สวัสดิการ (แว่นตา)", value: "welfare_glasses"},
+        {label: "สวัสดิการ (เลนแว่นตา)", value: "welfare_lenses"},
+        {label: "สวัสดิการ (ตรวจสุขภาพ)", value: "welfare_opd"},
+        {label: "เงินจ่ายล่วงหน้า (เล็กๆน้อยๆ)", value: "advance"},
+        {label: "ลงทุนในสินทรัพย์องค์กร", value: "asset"},
+        {label: "เงินค้ำประกัน", value: "guarantee"},
+      ];
+
       return (
       	<View style={styles.container}> 
       		<Navbar
             title="Add expense"
             lTitle="Back"
-            rTitle="Submit"
-            rHandler={()=>alert("Call submit function")}
             />
 
           <View style={styles.form}>
 
             <View style={styles.row}>
-              <View style={styles.labelRow}>
-                <Icon style={styles.icon} name="tags" size={23} color="#666" />
-                <Text>Amount</Text>               
+              <View style={styles.labelContainer}>
+                  <Icon style={styles.icon} name="money" size={23} color="#666" />
+                  <Text style={styles.label}>Amount</Text>     
               </View>
-              <View style={{flex: 0.7}}>
+              <View style={styles.inputContainer}>
                <TextInput
+                  keyboardType={'numeric'}
                   style={styles.textInput} 
                   placeholder={'Amount'}
                   placeholderTextColor={'#DDD'} />
@@ -34,31 +47,52 @@ class ExpenseAdd extends React.Component {
             </View>
 
             <View style={styles.row}>
-              <View style={styles.labelRow}>
+              <View style={styles.labelContainer}>
                 <Icon style={styles.icon} name="tags" size={23} color="#666" />
-                <Text>Category</Text>               
+                <Text style={styles.label}>Category</Text>               
               </View>
-              <View style={{flex: 0.7}}>
-               <TextInput
-                  style={styles.textInput} 
-                  placeholder={'Category'} 
-                  placeholderTextColor={'#DDD'} />
+              <View style={styles.inputContainer}>
+
+                  <Picker
+                    selectedValue={this.state.language}
+                    onValueChange={(lang) => this.setState({language: lang})}>
+                    { CATEGORIES.map((category)=>(
+                      <Picker.Item key={category} label={category.label} value={category.value} />
+                    ))}
+                  </Picker>
               </View>
             </View>
 
             <View style={styles.row}>
-              <View style={styles.labelRow}>
-                <Icon style={styles.icon} name="tags" size={23} color="#666" />
-                <Text>Description</Text>               
+              <View style={styles.labelContainer}>
+                <Icon style={styles.icon} name="calendar" size={23} color="#666" />
+                <Text style={styles.label}>Date</Text>               
               </View>
-              <View style={{flex: 0.7}}>
+              <View style={styles.inputContainer}>
                <TextInput
                   style={styles.textInput} 
-                  placeholder={'Description'}
+                  placeholder={'Calendar'} 
                   placeholderTextColor={'#DDD'} />
               </View>
             </View>
 
+            <View style={{flexDirection: 'column', marginTop: 20}}>
+              <View style={{flexDirection: 'row'}}>
+                <Icon style={styles.icon} name="commenting-o" size={23} color="#666" />
+                <Text style={styles.label}>Description</Text>               
+              </View>
+              <View style={{alignItems:'flex-start'}}>
+               <TextInput
+                  multiline={true}
+                  style={styles.textareaInput} 
+                  placeholder={'Description'}
+                  placeholderTextColor={'#DDD'} />
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.submit}>
+            <Text style={styles.fontWhite} > Submit </Text>
           </View>
       	</View>
       );
@@ -67,32 +101,48 @@ class ExpenseAdd extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#EAEDEE',
+    flex: 1
   },
   form: {
-    flex: 1,
-    flexDirection: 'column',
-    marginTop: 20,
-    backgroundColor: 'white',
+    padding: 5,
   },
   row: {
-    flex: 1,
-    flexDirection: 'row',
-    paddingLeft: 20,
-    paddingRight: 20,
-    height: 50,
+    flex: 1, 
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  column: {
+    flexDirection: 'column', 
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  labelContainer: {
+    flex: 0.3,
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  inputContainer: {
+    flex: 0.7,
+  },
+  textareaInput: {
+    height: 150,
+    textAlignVertical: 'top'
   },
   icon: {
-    marginRight: 20,
+    marginRight: 10,
   },
-  textInput: {
-
+  submit: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#900',
+    padding: 15,
+    marginTop: 20,
   },
-  labelRow: {
-    flex: 0.3, 
-    flexDirection: 'row',
-    justifyContent: 'center'
+  fontWhite: {
+    fontSize: 18,
+    color: '#fff'
   }
 });
 
